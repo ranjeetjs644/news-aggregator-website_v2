ed3acac167a81a706f7e003e3ef10f0bf5bfc11b
Ranjeet Patel <123392335+ranjeetjs644@users.noreply.github.com>
2024-11-24 19:22:44 +0530

    updated and created the logout and refresh-token  controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -3,6 +3,7 @@ import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 import { uploadOnCloudinary } from '../services/cloudinary.services.js'
+import jwt from 'jsonwebtoken'
 // import mongoose from 'mongoose'
 
 
@@ -161,4 +162,36 @@ const logoutUser = asyncHandler(async (req, res) => {
           .json(new ApiResponse(200, {}, "User logged out successfully"))
 })
 
-export { registerUser, loginUser, logoutUser }
+const refreshAccessToken = asyncHandler(async (req, res) => {
+     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+     if (!incomingRefreshToken) {
+          throw new ApiError(401, "Unauthorized request")
+     }
+
+     const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+     const user = await User.findById(decodedToken._id)
+     if (!user) throw new ApiError(401, "Invalid refreshToken")
+
+     try {
+          const { accessToken, refreshToken } = generateAccessAndRefreshToken(user._id)
+          const options = {
+               httpOnly: true,
+               secure: process.env.NODE_ENV === 'production'
+          }
+          return res
+               .status(200)
+               .cookie('accesssToken', accessToken, options)
+               .cookie('refreshToken', refreshToken, options)
+               .json(
+                    new ApiResponse(
+                         200,
+                         refreshToken,
+                         "Access token  and refresh token generated successfully"
+                    )
+               )
+     } catch (error) {
+          throw new ApiError(401, error?.message || 'Invalid refresh Token')
+     }
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from '../controllers/auth.controllers.js'
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from '../controllers/auth.controllers.js'
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -16,5 +16,6 @@ router.route('/login').post(loginUser)
 // secured routes
 
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/refresh-token').post(refreshAccessToken)
 
 export default router
